a00fd7d5bd8af4023311c26f3f5647a7c0bea0c3
NicholasPaniccioli <nicholaspanic20@gmail.com>
2022-10-30 19:25:58 -0400

    Breaks, Continues, & Labels!
    Learned the syntax and how to use 'breaks' and 'continues' in loops. Also learned about 'labels' and how to use them with nested loops.

---
+++ Branches-Loops/Label.java
@@ -0,0 +1,17 @@
+class Label {
+    public static void main(String[] args){
+        outerLoop : for(int i = 1; i < 4; i++){
+            for(int j = 1; j < 4; j++){
+                if(i==1 && j==1){
+                    System.out.println("Continues outerLoop when  i="+i+" j="+j);
+                    continue outerLoop;
+                }
+                if(i==2 && j==3){
+                    System.out.println("Breaks outerLoop when i=" +i+" j="+j);
+                    break outerLoop;
+                }
+                System.out.println("Running i="+i+" j="+j);
+            }
+        }
+    }
+}

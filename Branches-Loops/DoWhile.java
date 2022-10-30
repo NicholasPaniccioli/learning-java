9dd9f39f032925160664c90cde34b40ae63146da
NicholasPaniccioli <nicholaspanic20@gmail.com>
2022-10-30 19:08:59 -0400

    For, While, & DoWhile Loops
    Learned the syntax of the aforementioned loop structures

---
+++ Branches-Loops/DoWhile.java
@@ -0,0 +1,10 @@
+class DoWhile {
+    public static void main(String[] args){
+        int num = 100;
+
+        do{
+            System.out.println("DoWhile Countup: " + num);
+            num += 10;
+        } while(num < 10);
+    }
+}
---
+++ Branches-Loops/For.java
@@ -0,0 +1,14 @@
+class For {
+    public static void main (String[] args){
+        int num = 0;
+
+        for(int i = 1; i < 4; i++){
+            System.out.println("Outer Loop i=" + i);
+
+            for(int j = 1; j < 4; j++){
+                System.out.println("\tInner Loop j=" + j);
+                System.out.println("\t\tTotal num="+ (++num));
+            }
+        }
+    }
+}

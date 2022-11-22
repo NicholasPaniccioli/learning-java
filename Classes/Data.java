a756b4a4a9d816e6e37c987dc4c2ea4a5a9494c9
Nicholas Paniccioli <nicholaspanic20@gmail.com>
2022-11-21 19:47:20 -0500

    Continuing With Classes
    Exercise involved a java script to use methods from other java scripts and classes.

---
+++ Classes/Data.java
@@ -0,0 +1,8 @@
+class Data {
+    public final static String txt = "This is a global variable in the Data class";
+    
+    public static void greeting(){
+        System.out.print("This is a global method");
+        System.out.println(" of the Data class");
+    }
+}
---
+++ Classes/Multi.java
@@ -0,0 +1,10 @@
+class Multi {
+    public static void main (String[] args){
+        String msg = "This is a local variable in the Multi class";
+        System.out.println(msg);
+
+        Data.greeting();
+        
+        Draw.line();
+    }
+}

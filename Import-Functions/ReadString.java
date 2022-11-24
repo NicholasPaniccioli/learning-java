3f85a18b334c4edcb9e3b62c4b912aede6dc5ecf
NicholasPaniccioli <nicholaspanic20@gmail.com>
2022-11-24 15:10:47 -0500

    Learning To Handle Files
    Went over File object and how to get user input from the console using InputStreamBuffer & BufferedReader.

---
+++ Import-Functions/ReadString.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class ReadString {
+    public static void main(String[] args){
+        System.out.print("Enter the title of this book: ");
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(isr);
+
+        String input = "";
+
+        try{
+            input = buffer.readLine();
+            buffer.close();
+        } catch (IOException e){
+            System.out.println("An input error has occured");
+        }
+
+        System.out.println("\nThanks, you are reading " + input);
+    }
+}
